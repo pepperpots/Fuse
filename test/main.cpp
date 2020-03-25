c8053ba73f3819685b8a79fcccb345ec00481d07
Richard Neill <richard.neill@manchester.ac.uk>
2020-03-25 17:24:31 +0000

    [Test] Initial commit of gtest library and cmake setup

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+/* Tests:
+*		
+		- Check that a log file is created at the right place when main is run (move main out?)
+		- Check that run_options results in an exception if no option given within main options or utility options 
+		- Check that any of the three utility options only work if both tracefile and benchmark given
+		- Check that any of the main options only work if target_dir is given
+		- Check that a new log file is created in the fuse target directory if a main option is given
+*
+*
+*/
